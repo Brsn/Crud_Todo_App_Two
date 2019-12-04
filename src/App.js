99276f6c978316d3ef3ebba2409bc0c88f0f92e4
Bronson Brown <Brsn90@icloud.com>
2019-12-04 15:28:30 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from "./components/create-todo";
+import EditTodo from "./components/edit-todo";
+import TodosList from "./components/todos-list";
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <h2>Todo App</h2>
+        </div>
+        <Route path="/" exact component={TodosList} />
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
+      </Router >
+    );
+  }
+}
+
+export default App;
---
+++ src/components/create-todo.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import axios from "axios";
+
+export default class TodosList extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todo_name: '',
+            todo_description: '',
+            todo_completed: false
+        }
+        this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
+        this.onChangeTodoName = this.onChangeTodoName.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+    onChangeTodoDescription(e) {
+        this.setState({
+            todo_description: e.target.value
+        });
+    }
+    onChangeTodoName(e) {
+        this.setState({
+            todo_name: e.target.value
+        });
+    }
+    onSubmit(e) {
+        e.preventDefault();
+
+        console.log(`Form submitted:`);
+        console.log(`Todo Name: ${this.state.todo_name}`);
+        console.log(`Todo Description: ${this.state.todo_description}`);
+        console.log(`Todo Completed: ${this.state.todo_completed}`);
+
+        const newTodo = {
+            todo_name: this.state.todo_name,
+            todo_responsible: this.state.todo_description,
+            todo_completed: this.state.todo_completed
+        }
+
+        axios.post("http://localhost:4000/todos/add", newTodo)
+            .then(res => console.log(res.data));
+        this.setState({
+            todo_name: '',
+            todo_description: '',
+            todo_completed: false
+        })
+    }
+    render() {
+        return (
+            <div style={{ marginTop: 20 }}>
+                <h3>Create New Todo</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Name: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.todo_name}
+                            onChange={this.onChangeTodoName}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Description: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.todo_description}
+                            onChange={this.onChangeTodoDescription}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create Todo" className="btn btn-primary" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
