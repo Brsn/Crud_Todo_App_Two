44e2e5e8bf35a49898bf71ab990e7f1be39c7613
Bronson Brown <Brsn90@icloud.com>
2020-01-16 12:42:06 -0700

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,13 @@ let Todo = require('./todo.model');
 
 //middleware
 app.use(cors());
+app.all('*', function(req, res, next) {
+    var origin = req.get('origin'); 
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 app.use(bodyParser.json());
 
